5b95291e5e85574cc1edad30a83a71849d683864
Christian Pari <ChristianPari@outlook.com>
2020-09-08 18:32:00 -0400

    Added varaibles and reorganized code

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -1,50 +1,39 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const validateUser = require('../../middleware/validateUser');
-const checkUserCreds = require('../../middleware/checkUserCreds');
-const createJWT = require('../../middleware/createJWT');
+const validateUser = require("../../middleware/validateUser");
+const checkUserCreds = require("../../middleware/checkUserCreds");
+const createJWT = require("../../middleware/createJWT");
 
-const User = require('../../models/User');
+const User = require("../../models/User");
 
 //@path: POST *server*/user/register
 //@desc: handels user registration, will upload a new User document to MongoDB.
-//@access: public 
-router.get(
-    '/register', 
-    validateUser, 
-    (req, res) => {
-        try {
-            //req.userData is defined by the validation middleware
-            const newUser = await User.create(req.userData);
-            res.status(201).json(newUser);
-
-        } catch (err) {
-            res.status(500).json({
-                message: err.message,
-                error: err
-            })
-        }
+//@access: public
+router.get("/register", validateUser, async(req, res) => {
+    try {
+        //req.userData is defined by the validation middleware
+        const newUser = await User.create(req.userData);
+        res.status(201).json(newUser);
+    } catch (err) {
+        res.status(500).json({
+            message: err.message,
+            error: err,
+        });
     }
+});
 
-)
-
-//@desc put/login a user and respond with JWT containing their user DB document _id 
+//@desc put/login a user and respond with JWT containing their user DB document _id
 //@path *server*/user/login
 //@access public
-router.put(
-    "/login", 
-    checkUserCreds,
-    createJWT,
-    (req, res) => {
-        try {
-            res.json(req.token);
-        } catch (err) {
-            res.status(500).json({
-                message: err.message,
-                error: err
-            })
-        }
+router.put("/login", checkUserCreds, createJWT, (req, res) => {
+    try {
+        res.json(req.token);
+    } catch (err) {
+        res.status(500).json({
+            message: err.message,
+            error: err,
+        });
     }
-)
+});
 
-module.exports = router
+module.exports = router;
--- database/mongo-connect.js
+++ database/mongo-connect.js
@@ -1,27 +1,31 @@
 const mongoose = require("mongoose");
 
 module.exports = (uri) => {
-  mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  });
+    mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    });
 
-  mongoose.connection.on("open", () => {
-    console.log(`connected to Database`);
-  });
+    mongoose.connection.on("open", () => {
+        console.log(`Connected to Database`);
+    });
 
-  mongoose.connection.on("error", (err) => {
-    console.error(`\nERROR Connecting To Database: ${err}\n`);
-  });
+    mongoose.connection.on("error", (err) => {
+        console.error(`\nERROR Connecting To Database: ${err}\n`);
+    });
 
-  mongoose.connection.on("connected", (err) => {
-    console.log(
-        `\nConnecting To Database\n`
-    );
-  });
+    mongoose.connection.on("connected", (err) => {
+        console.log(
+            `\nConnecting To Database\n`
+        );
+    });
 
-  mongoose.connection.on("disconnected", (err) => {
-    console.error(`\nThe application has been disconnected from the database;\n\nError With DB: ${err.message || err}\n`);
-  });
+    mongoose.connection.on("disconnected", (err) => {
+
+        if (err !== undefined) {
+            console.error(`\nThe application has been disconnected from the database;\n\nError With DB: ${err.message || err}\n`);
+        }
+
+    });
 };
