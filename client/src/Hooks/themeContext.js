a9521591fd4429d14a298918aeec8ce1325c74c9
Christian Pari <ChristianPari@outlook.com>
2020-09-19 17:35:36 -0400

    created custom hook for theming using useContext, and useState, gloabbly holds state and passes to children components. Dark mode theming file with styling for elements

---
+++ client/src/Hooks/themeContext.js
@@ -0,0 +1,34 @@
+// custom theme handler
+
+import React, { useContext, useState } from 'react'
+import useLocalStorage from './useLocalStorage'
+
+const ThemeContext = React.createContext()
+const ThemeUpdateContext = React.createContext()
+
+export function useTheme() {
+  return useContext(ThemeContext)
+}
+
+export function useThemeUpdate() {
+  return useContext(ThemeUpdateContext)
+}
+
+export function ThemeProvider({ children }) {
+
+  const [theme, setTheme] = useLocalStorage('darkTheme', false)
+  const [darkTheme, setDarkTheme] = useState(theme)
+
+  function toggleTheme() {
+    setTheme(theme => !theme)
+    setDarkTheme(prevDarkTheme => !prevDarkTheme)
+  }
+
+  return (
+    <ThemeContext.Provider value={darkTheme}>
+      <ThemeUpdateContext.Provider value={toggleTheme}>
+        {children}
+      </ThemeUpdateContext.Provider>
+    </ThemeContext.Provider>
+  )
+}
