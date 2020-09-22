b679f104d7b8c2f818234c5cd2936dfb7be5007c
Christian Pari <ChristianPari@outlook.com>
2020-09-21 21:16:26 -0400

    Wrote logic so the React components renders correctly depending on the data of a user being logged in or not

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -16,6 +16,7 @@ export default function Home(props) {
 
 	const loginLink = "/login"
   const regLink = "/register"
+  const loggedInUser = sessionStorage.getItem('user')
   const darkTheme = useTheme()
   const themeStyles = darkModeTheming(darkTheme)
   const [visits, setVisits] = useLocalStorage('visits', {})
@@ -25,29 +26,49 @@ export default function Home(props) {
     setVisits({...visits, 'home': visits.home})
   }
 
+  if (loggedInUser) {
+    document.title = loggedInUser
+  }
+
 	return (
     <div 
       className="home"
       style={themeStyles.text}
     >
-			<h1>Welcome to our Service</h1>
-			<p>
-				<big>Need an account? </big>
-        <Button 
-          className="register_button"
-          text={"Sign Up"}
-          onClick={() => {window.location = regLink}}
-          style={themeStyles.btn}
-        />
-				<br />
-				<big>Already a user? </big>
-        <Button 
-          className="login_button"
-          text={"Login"}
-          onClick={() => {window.location = loginLink}}
-          style={themeStyles.btn}
-        />
-			</p>
+			<h1>{loggedInUser? `Welcome, ${loggedInUser}!` : 'Welcome to Our Service'}</h1>
+			{
+        !loggedInUser ?
+
+        <div className='user_actions'>
+          <big>Need an account? </big>
+          <Button 
+            className="register_button"
+            text={"Sign Up"}
+            onClick={() => {window.location = regLink}}
+            style={themeStyles.btn}
+          />
+          <br />
+          <big>Already a user? </big>
+          <Button 
+            className="login_button"
+            text={"Login"}
+            onClick={() => {window.location = loginLink}}
+            style={themeStyles.btn}
+          />
+        </div> :
+        
+        <div className='user_welcome'>
+          <Button 
+            className='logout_button'
+            text={'Logout'}
+            onClick={() => {
+              sessionStorage.removeItem('user')
+              window.location = '/'
+            }}
+            style={themeStyles.btn}
+          />
+        </div>
+      }
       <ResetVisits 
         btnTheme={themeStyles.btn}
         btnOnClick={setVisits}
