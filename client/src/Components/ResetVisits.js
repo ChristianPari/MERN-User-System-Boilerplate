271d8eb9991b50cc91af0a18efa081b3e50c873f
Christian Pari <ChristianPari@outlook.com>
2020-09-22 21:03:09 -0400

    Recreated slightly and implemented local storage hook to access the visists property to alter it via btn onclicks

--- client/src/Components/ResetVisits.js
+++ client/src/Components/ResetVisits.js
@@ -3,22 +3,27 @@ import React from 'react'
 // components
 import Button from './Button'
 
+// hooks
+import useLocalStorage from '../Hooks/useLocalStorage'
+
 export default function ResetVisits(props) {
+
+  const [visits, setVisits] = useLocalStorage('visits', {})
 
   return (
     <div className='resets'>
       <Button 
         className='resetVisits'
         text={'Reset ALL Visits'}
-        onClick={() => props.btnOnClick({})}
+        onClick={() => setVisits({})}
         style={props.btnTheme}
         />
       <Button 
         className='resetVisits'
         text={`Reset ${props.pageName.toUpperCase()} Visits`}
         onClick={() => {
-          if (props.pageVisits[props.pageName]) delete props.pageVisits[props.pageName] 
-          props.btnOnClick({...props.pageVisits})
+          if (visits[props.pageName]) delete visits[props.pageName] 
+          setVisits({...visits})
         }}
         style={props.btnTheme}
       />
